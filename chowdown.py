51004b9ee3da4c3aac7562098fae1019d81eba33
Afats <mmdohad@gmail.com>
2023-03-12 21:47:23 +1100

    inital move scrape logic

---
+++ chowdown.py
@@ -0,0 +1,257 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent 
+import time
+import random
+import re
+
+
+# create a random real user agent to act as a spectator - avoids 0 users and no logs showing when scraping
+ua = UserAgent()
+userAgent = ua.random
+# print(userAgent)
+
+options = Options()
+options.add_argument(f'user-agent={userAgent}')
+options.add_experimental_option("detach", True)
+
+# driver = webdriver.Chrome(options=options)
+# battle_link = "https://play.pokemonshowdown.com/battle-gen9randombattle-1820033851"
+# driver.get(battle_link)
+# # initally wait min. seconds for page to load
+# driver.implicitly_wait(7)
+
+
+battle_link = ""
+all_battle_logs = []
+team = {}
+challenger = ""
+acceptor = ""
+
+# parse input battle link
+def parse_battle_link(battle_link):
+    # remove leading and trailing whitespace
+    battle_link = battle_link.strip()
+    # if link doesn't start with https, add https://play.pokemonshowdown.com/
+    if not battle_link.startswith("https://play.pokemonshowdown.com/"):
+        battle_link = "https://play.pokemonshowdown.com/" + battle_link
+
+    return battle_link
+    
+
+
+
+
+# helper function: get turn number from turn string
+# Ex. get "x" from "Turn x" using split()
+def get_turn_num(turn_string):
+    try: 
+        return int(turn_string.split()[1])
+    except:
+        return -1
+
+
+# helper function: scrape the turn number
+def scrape_turn(i):
+    try: 
+        turn = driver.find_element(By.XPATH, f"//div[@class='inner message-log']/h2[@class='battle-history'][{i}]")
+        return get_turn_num(turn.text)
+    except:
+        print("no turn found")
+        return 0
+    
+
+# get the current turn in play
+def get_curr_game_turn(battle_logs):
+    try:
+        # get the last element in battle_logs, which is the current turn in play
+        curr_game_turn = get_turn_num(battle_logs[-1])
+        print("Current game turn:", curr_game_turn)
+        return curr_game_turn
+    except:
+        print("No current turn found. :(")
+        return 0
+
+
+
+# get challenger and acceptor usernames
+def get_players():
+    global challenger
+    global acceptor
+    try:
+        # scrape the player's username
+        player = driver.find_element(By.XPATH, "//div[@class='trainer trainer-near']")
+        challenger = player.text
+        print("Challenger:", challenger.strip())
+        team[challenger] = {}
+        # scrape the opponent's username
+        player = driver.find_element(By.XPATH, "//div[@class='trainer trainer-far']")
+        acceptor = player.text
+        print("Acceptor:", acceptor.strip())
+        print()
+        team[acceptor] = {}
+        return 1
+    
+    except:
+        print("No players found. :(")
+        return 0
+
+
+# scrape until curr_turn is curr_game_turn
+def scrape_battle_logs(curr_turn, curr_game_turn):
+    try:
+        action_logs_scraped = driver.find_elements(By.XPATH, "//div[@class='inner message-log']//div[@class='battle-history']/strong[1] | //div[@class='inner message-log']//h2[@class='battle-history']")
+        action_logs = []
+        
+        for log in action_logs_scraped:
+            action_logs.append(log.text)
+
+        action_logs.insert(0, "Turn 0")
+        print("Actions: ", action_logs)
+        print()
+
+        # only 2 actions max per turn - move/switch poke, and move/switch poke (note: move itself may have multiple actions)
+        # scrape b/w curr_turn and curr_game_turn
+        x = -3
+        y = 0
+        for turn in range(curr_turn, curr_game_turn+1):
+            # set starting challenger and acceptor pokemon
+            x += 3
+            y += 3
+
+            # for turn 0, set challenger and acceptor pokemon
+            if (x == 0):
+                for j in range(x,y):
+
+                    # all turns until current turn parsed
+                    if j == x and get_turn_num(action_logs[j]) == curr_game_turn:
+                        break
+                        
+                    elif j == x+1:
+                        # challenger's pokemon
+                        team[challenger][action_logs[j]] = {}
+                    elif j == x+2:
+                        # acceptor's pokemon
+                        team[acceptor][action_logs[j]] = {}
+            
+            # for all other turns, parse the logs as moves/switches
+            else:
+                continue
+                # call parse fucntion logic for moves
+                    # parse all_battle_logs to remove common english words (except for turn/Turn, playernames and numbers)
+                    # check instance of word in all_battle_logs[i]
+                # call parse function logic for switches if moves are not found
+
+    except:
+        print("No turn battle logs found. :(")
+        return 0
+    
+      
+
+# get all battle logs from the start of the battle to the current turn in play
+def get_all_battle_logs():
+    try:
+        battle_logs_scraped = driver.find_elements(By.XPATH, "//div[@class='inner message-log']/*[self::div or self::h2][not(@class='spacer battle-history')]")
+        # print(battle_logs_scraped.text)
+        battle_logs = []
+        for log in battle_logs_scraped:
+            battle_logs.append(log.text)
+
+        battle_logs.insert(0, "Turn 0")
+        print("Battle logs: ", battle_logs)
+        print()
+        return battle_logs
+    except:
+        print("No battle logs found. :(")
+        return 0
+    
+
+
+# current turn being scraped
+curr_turn = 0
+# the current turn in play -- may not contain battle logs
+curr_game_turn = 0
+
+# run main function every 4-7 seconds
+def main():
+    global curr_turn
+    global curr_game_turn
+    global battle_link
+    global all_battle_logs
+    
+    print("Scraping...\n")
+    get_players()
+    all_battle_logs = get_all_battle_logs()
+    curr_game_turn = get_curr_game_turn(all_battle_logs)
+    scrape_battle_logs(curr_turn, curr_game_turn)
+    
+    while True:
+        # all_battle_logs = get_all_battle_logs()
+        # curr_game_turn = get_curr_game_turn(all_battle_logs)
+        # scrape_battle_logs(curr_turn, curr_game_turn)
+        # pick a random value between x and y seconds
+        time_to_wait = random.SystemRandom().uniform(4.1,8.3)
+        time.sleep(time_to_wait)
+        
+
+battle_link = input("Insert battle link or code: ")
+battle_link = parse_battle_link(battle_link)
+driver = webdriver.Chrome(options=options)
+driver.get(battle_link)
+# initally wait min. seconds for js in page to load
+driver.implicitly_wait(7)
+main()
+
+
+
+# Business logic:
+# ----------------
+# NOTE: spectator always sees from challenger's perspective
+# spectator scrape (only works when both players send out their starting pokemon for now -- write seperate setup code for battle factory):
+    # 0. get battle link/code
+    # 0.5. get battle type (random battle, battle factory, etc.)
+    # 1. get both player names
+    # 1.5: ask user if they're challenger or acceptor
+    # 2. get all_battle_logs so far
+    # 3. curr_game_turn: get the curr turn being played from end of all_battle_logs (Ex. if turn 6 is curr_game_turn, then the battle is currently on turn 6 and is yet to finish)
+    # 4. scrape the battle logs for each curr_turn, where curr_turn <= curr_game_turn. currr_turn is initially 0 (Turn 0 is pokemon being sent out). 
+        # 4.1. for turn 0, assign starting pokemon to each player (based on near/far trainer and starting pokemon text order)
+        # 4.2. for [turn 1, curr_game_turn-1] (inclusive), compare turn logs with battle logs to get:
+        #   4.2.1 the pokemon that was sent out and assign to either challenger or acceptor
+        #   4.2.2 get moves used in the turn and assign to either challenger's pokemon or acceptor pokemon (max 4 moves per pokemon)
+        #   4.2.3 add metadata to each pokemon object (Ex. type, moves, etc.) / update accordingly
+                # get pokemon speed range from pokeapi
+                # get pokemon type from pokeapi
+                # get pokemon type/s weaknesses from pokeapi
+                # get pokemon type/s resistances from pokeapi
+                # get pokemon type/s immunities from pokeapi
+                # get pokemon move counts, type and category from pokeapi
+    
+    # 5. for current game turn:
+        # display the current game turn
+        # display both player's revealed pokemon, and moves used
+        # display the current game state: 
+            #  challenger's pokemon, acceptor's pokemon
+            #  challenger's pokemon's moves, acceptor's pokemon's moves under 2 adjacent columns
+            #  highlight pokemon w faster speed w lightining bolt emoji / some color
+            #  highlight pokemon w a type/s matchup advantage w a star emoji / color
+            #  highlight moves w a type/s 2x matchup advantage w a star emoji / color
+            #  highlight moves w a type/s 4x matchup advantage w a skull emoji / color
+            #  highlight moves w a type/s 0x matchup advantage (immunity to it) w a 0 emoji / color
+            #  highlight moves w a type/s 0.5x matchup advantage (resistance to it) w a 1/2 emoji / color
+        # display recommended move/s for each pokemon
+            # if no 4x/2x/neutral moves available, recommended withdrawing/status?
+
+
+
+
+     
+
+
+
+
+    
+
+        
+
---
+++ showdown_helper_bot.py
@@ -0,0 +1,97 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent 
+
+# just a py page for ideas, experiemnts and code snippets
+
+# todo: upload to github and add a descriptive readme for setup (install python, install selenium, install fake_useragent, install chromedriver, etc.)
+# todo: get battle link/code from user
+# todo: ask user if they are challenger or acceptor 
+# todo: scrape from user's session instead of spectator if possible
+# todo: write seperate setup code for battle factory
+# todo: extract poke stuff on hover over pic??
+# todo: use tooltip stats to make better naive predictions
+# todo: cool user interface to enter battle link and scrape
+# todo: send put request to current battle link as player (instead of just scraping as spectator)
+# todo: get ditto's type/s from poke's bar
+# endgame todo: keep track of any pokemon type changes from chat - ex. terastallizing, mega evolving, etc.
+
+
+
+# rotate through IP addresses to avoid getting banned from pokemon showdown
+
+
+# add random clicks, scrolls, and mouse movements to make your crawling seem less predictable. 
+# also add random delays between actions to make it look more human-like.
+
+# reduce scraping speed
+
+# endgame: use headless chrome to avoid opening a browser window -- see if this works
+
+
+# create a random real user agent to act as a spectator - avoids 0 users and no logs showing when scraping
+ua = UserAgent()
+userAgent = ua.random
+# print(userAgent)
+
+options = Options()
+options.add_argument(f'user-agent={userAgent}')
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options)
+
+battle_link = "https://play.pokemonshowdown.com/battle-gen9randombattle-1820046302"
+driver.get(battle_link)
+
+# initally wait min. seconds for page to load
+driver.implicitly_wait(7)
+
+
+# scrape inner message-log div from battle link
+# use until.elementLocated() ?
+print("scraping...")
+
+
+# turns are in h2 tags
+print([turn.text for turn in driver.find_elements(By.XPATH, "//div[@class='inner message-log']//h2[@class='battle-history']")])
+
+# get battle log for each turn
+# if pokemon is in battle log, it was sent out
+# check what poekmon was mentioned in the move and add move to opponent or user dictionary
+     # alt. check tooltip for opponent pokemon move deets
+print([battle_log.text for battle_log in driver.find_elements(By.XPATH, "//div[@class='inner message-log']//div[@class='battle-history']/strong[1]")])
+
+
+battle_logs = driver.find_elements(By.XPATH, "//div[@class='inner message-log']/*[self::div or self::h2][not(@class='spacer battle-history')]")
+for log in battle_logs:
+    print(log.text)
+
+battle_logs.insert(0, "Turn 0")
+
+# usernames in div class "trainer trainer-near" 
+# opponent usernames in div class "trainer trainer-far"
+
+# for battle factory/similar (doesn't apply to random battles/similar)
+    # afats's team:
+        # Yveltal / Rayquaza / Heatran / Magearna / Palkia / Marshadow
+    # bruvvvvvvvvvvv's team:
+        # Palkia / Landorus / Lunala / Magearna / Ho-Oh / Necrozma-Dusk-Mane
+# OR "teamicons" class inside has pokemon names to get pokemon names? 
+# active pokemon deets in "controls" and "tooltip tooltip-activepokemon" divs
+# active pokemon sent out by user and opponent in div aria-label "Active Pokemon" and 
+# all active pokemon deets and type (incl. terra type) in "tooltip tooltip-activepokemon" 
+# hover over opponent pokemon to get their deets in "tooltip tooltip-activepokemon" 
+# scrape all of my inactive pokemon from "chooseSwitch" button names
+
+
+# keep track of last turn scraped
+# continously scrape the battle link for new messages every 4-7 seconds or until turn x elementLocated
+    # if turns have passed, scrape the latest turn
+    
+    # extract the pokemon names and moves used (usually before an "!" and in bold) OR fom tooltips
+    # use pokeapi to get the pokemon types move type and info
+    # record the pokemon and move used in 2 dictionaries (user and opponent)
+    # print battling pokemon, types and moves used
+    # if pokekon has a weak type match-up to opponent, suggest withdrawing pokemon
+    # print a recommendation for the next move to use depending on opponent pokemon: super effective, not very effective, neutral, etc.
